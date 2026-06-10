2f3406180cfa21b3c60ed8fad311d83c25ab0bc0
millionaire6969 <mulekrrish@gmail.com>
2026-06-10 09:44:18 +0530

    Update scanner.py

--- scanner.py
+++ scanner.py
@@ -1,1 +1,15 @@
+import requests
 
+url = "https://api.gateio.ws/api/v4/spot/tickers"
+
+response = requests.get(url)
+
+data = response.json()
+
+print("Total Coins:", len(data))
+
+for coin in data[:10]:
+    print(
+        coin["currency_pair"],
+        coin["last"]
+    )
